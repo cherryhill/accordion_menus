adffa9252db599c3956e9679dfbd1f657fa5b75b
Biswajit Mondal <bisw271923@gmail.com>
2019-02-20 19:53:19 +0530

    add help hook

--- js/accordion_menus.js
+++ js/accordion_menus.js
@@ -1,5 +1,6 @@
 (function ($) {
   $(document).ready(function () {
+    'use strict';
     $('.accordion_menus_block_container').accordion({header: 'h2.accordion-title', autoHeight: false});
   });
 })(jQuery);
