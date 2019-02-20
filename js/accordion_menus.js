4404b3e7ec5d4e05c2772dfe7aacdfc5029bb17c
Biswajit Mondal <bisw271923@gmail.com>
2019-02-20 20:02:44 +0530

    use namespace

--- js/accordion_menus.js
+++ js/accordion_menus.js
@@ -1,6 +1,7 @@
+'use strict';
+
 (function ($) {
   $(document).ready(function () {
-    'use strict';
     $('.accordion_menus_block_container').accordion({header: 'h2.accordion-title', autoHeight: false});
   });
 })(jQuery);
